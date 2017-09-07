5fc5538fb84de343c034bd86929906d17a8f7837
張 <oliverchang@utexas.edu>
2017-09-06 22:03:58 -0500

    parallax_node refactor + scarecorw

--- godot/levels/forest/forest.gd
+++ godot/levels/forest/forest.gd
@@ -6,15 +6,17 @@ const FOREGROUND = preload("res://levels/forest/foreground.png")
 const BACKGROUND = preload("res://levels/forest/background.png")
 
 const TREE = preload("res://levels/forest/objects/Tree.tscn")
+const SCARECROW = preload("res://levels/forest/objects/Scarecrow.tscn")
 
 #environment
-var tiles
-var tiles_sky
-var base_background
-var base_foreground
+var container_ground
+var container_sky
+var container_background
+var container_foreground
 
 #obstacles
 var trees
+var scarecrows
 
 #misc
 var girl
@@ -30,9 +32,6 @@ func _ready():
 	viewport_width = get_viewport_rect().size.x
 	var ground_width = GROUND.get_width()
 	
-	tiles = []
-	tiles_sky = []
-	
 	#print(viewport_width)
 	#print(background_width)
 	
@@ -40,53 +39,30 @@ func _ready():
 	var max_range = min_range*10
 	print("[forest.tscn]: range(%s,%s)" % [-min_range, max_range])
 	
+	# how far to keep spawning obstacles
 	var max_dist = max_range*SKY.get_width()
 	#print(max_dist)
 	
-	for x in range(-min_range, max_range):
-		var sky = Sprite.new()
-		sky.set_texture(SKY)
-		sky.set_pos(Vector2(x*SKY.get_width(),0))
-		add_child(sky)
-		tiles_sky.append(sky)
+	container_sky = _add_parallax(1, 1, SKY, 0)
+	container_background = _add_parallax(min_range, max_range, BACKGROUND, 50)
 
-	base_background = Node2D.new()
-	for x in range(-min_range, max_range):
-		var tile = Sprite.new()
-		tile.set_texture(BACKGROUND)
-		tile.set_pos(Vector2(x*BACKGROUND.get_width(),0))
-		base_background.add_child(tile)
-	add_child(base_background)
-
-	trees = _add_obstacles(0, max_dist, TREE)
-
-	# floor layer over everything else
-	for x in range(-min_range, max_range):
-		var tile = Sprite.new()
-		tile.set_texture(GROUND)
-		tile.set_pos(Vector2(x*GROUND.get_width(),0))
-		add_child(tile)
-		tiles.append(tile)
-	
+	trees = _add_obstacles(0, max_dist, TREE, 100)
+	scarecrows = _add_obstacles(0, max_dist, SCARECROW, 100)
 	girl.set_z(100)
 	
-
-	base_foreground = Node2D.new()
-	for x in range(-min_range, max_range*2):
-		var tile = Sprite.new()
-		tile.set_texture(FOREGROUND)
-		tile.set_pos(Vector2(x*FOREGROUND.get_width(),0))
-		base_foreground.add_child(tile)
-	add_child(base_foreground)
-	base_foreground.set_z(200)
+	container_ground = _add_parallax(min_range, max_range, GROUND, 150)
+	container_foreground = _add_parallax(min_range, max_range*2, FOREGROUND, 200)
 	
 	set_process(true)
 
 func _process(delta):
-	base_background.translate(Vector2(delta*100,0))
+	container_foreground.translate(Vector2(-delta*600,0))
+	#container_ground.translate(Vector2(delta*0))
+	container_background.translate(Vector2(delta*400,0))
+	container_sky.translate(Vector2(delta*490,0))
 	girl.translate(Vector2(delta*500, 0))
-	base_foreground.translate(Vector2(-delta*600,0))
 	_update_obstacles(trees)
+	_update_obstacles(scarecrows)
 	#print(girl.get_pos().x)
 	#print(delta)
 
@@ -103,7 +79,7 @@ func _update_obstacles(var obstacles):
 	while (obstacles[0].get_pos().x < current_position-viewport_width):
 		obstacles.pop_front()
 
-func _add_obstacles(var start, var end, var obstacle_preload):
+func _add_obstacles(var start, var end, var obstacle_preload, var z_index):
 	var obstacles = []
 	var last_obstacle = start
 	while (last_obstacle < end):
@@ -111,6 +87,18 @@ func _add_obstacles(var start, var end, var obstacle_preload):
 		#print(last_obstacle)
 		var obstacle = obstacle_preload.instance()
 		obstacle.set_pos(Vector2(last_obstacle, 0))
+		obstacle.set_z(z_index)
 		add_child(obstacle)
 		obstacles.append(obstacle)
 	return obstacles
+
+func _add_parallax(var widths_left, var widths_right, var texture, var z_index):
+	var parallax_base = Node2D.new()
+	for x in range(-widths_left, widths_right):
+		var tile = Sprite.new()
+		tile.set_texture(texture)
+		tile.set_pos(Vector2(x*texture.get_width(),0))
+		parallax_base.add_child(tile)
+	parallax_base.set_z(z_index)
+	add_child(parallax_base)
+	return parallax_base
